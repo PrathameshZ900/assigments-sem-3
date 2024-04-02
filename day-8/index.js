e01adb95109ea7f4ee254c7cb3913b50b1909f13
prathamesh magar <prathameshma1@gmail.com>
2024-04-03 01:06:07 +0530

    ok

---
+++ day-8/index.js
@@ -0,0 +1,26 @@
+// Student Report Card 
+// Printing strings in the console
+
+var StdName="Рⷬrͬaͣᴛⷮhͪaͣmͫeͤs͛hͪ";
+var StdSchoolName="𝙎𝙝𝙧𝙞 𝙎𝙝𝙞𝙫𝙖𝙟𝙞 𝘾𝙤𝙡𝙡𝙖𝙜𝙚, 𝙋𝙖𝙧𝙗𝙝𝙖𝙣𝙞.";
+var StdGrade="🅰";
+var StdRollNo="cap04_025";
+var StdClass="12th";
+var StdScience=89;
+var StdMath=93;
+var StdEnglish=99;
+var StdTotalMarks= StdScience+StdMath+StdEnglish;
+
+console.log("Student Name:- ",StdName);
+console.log("Student School Name:- ",StdSchoolName);
+console.log("Students Grade:- ",StdGrade);
+console.log("Student Roll Number:- ",StdRollNo);
+console.log("Class:- ",StdClass);
+console.log("▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ");
+console.log("Students Marks")
+console.log("▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ");
+console.log("Science:-",StdScience,"English:- ",StdEnglish, "Math:- ",StdMath);
+console.log("Total Marks Of ",StdName,":-",StdTotalMarks);
+
+
+
