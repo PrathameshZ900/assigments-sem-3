0a28c3290f45766e069b6b8acf14b6f4b0278cee
prathamesh magar <prathameshma1@gmail.com>
2024-04-03 01:15:09 +0530

    ok

---
+++ day-8/index1.js
@@ -0,0 +1,8 @@
+// User Detail Types
+// Printing strings in the console 
+// In this assignment, you will practice printing strings in the console using JavaScript.
+
+var Name="Prathamesh";
+var Age=19;
+
+console.log("Name:- ", Name ,"Age:- ",Age)
