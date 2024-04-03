819cd99fc1752df37126eb357bceaf775beed2a9
prathamesh magar <prathameshma1@gmail.com>
2024-04-03 12:53:06 +0530

    ok done second tast

--- day-8/index1.js
+++ day-8/index1.js
@@ -2,7 +2,14 @@
 // Printing strings in the console 
 // In this assignment, you will practice printing strings in the console using JavaScript.
 
-var Name="Prathamesh";
-var Age=19;
+const { type } = require("os");
 
-console.log("Name:- ", Name ,"Age:- ",Age)
+// Create a variable to store your name
+var Name = "Prathamesh";
+
+// Create a variable to store your age
+var Age = 19;
+
+// Print the values stored in the variables along with their types
+console.log("Name:- ", Name, "（ Type Of Name:- ", typeof Name," ）");
+console.log("Age:- ", Age, " （ Type Of Age:- ", typeof Age," ）");
