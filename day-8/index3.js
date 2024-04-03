f52b81cace822a45d6439f47b6e9736b9d6d5524
prathamesh magar <prathameshma1@gmail.com>
2024-04-03 13:05:14 +0530

    done all problems

---
+++ day-8/index3.js
@@ -0,0 +1,15 @@
+// Title
+// Masai Tagline
+
+// Overview
+// In this assignment, you will practice printing strings in the console using JavaScript.
+
+// Detailed Explanation
+// Topics
+// Printing strings in the console
+// Problem Statement
+// Print "Masai School" in the console followed by "A Transformation in Education" in the next line.
+
+
+console.log("Mⷨaͣs͛aͣiͥ S͛cͨhͪoͦoͦl")
+console.log("🅰 🆃🆁🅰🅽🆂🅵🅾🆁🅼🅰🆃🅸🅾🅽 🅸🅽 🅴🅳🆄🅲🅰🆃🅸🅾🅽")
