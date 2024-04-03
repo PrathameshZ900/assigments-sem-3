42c1fa2fd7913123ceef69487211fd55a8eb6370
prathamesh magar <prathameshma1@gmail.com>
2024-04-03 13:00:37 +0530

    OK aswell done third task

---
+++ day-8/index2.js
@@ -0,0 +1,22 @@
+// F̳A̳M̳I̳L̳Y̳ N̳A̳M̳E̳S̳
+console.log(`F̳A̳M̳I̳L̳Y̳ N̳A̳M̳E̳S̳`);
+
+// In this assignment, you will practice printing strings in the console using JavaScript.Printing strings in the console.
+
+// Define a variable called name and assign your name as its value
+var name = "Prathamesh";
+
+// Print the value stored in the variable name
+console.log(name);
+
+// Change the value of the variable name to your father's name
+name = "Uddhav";
+
+// Print the updated value stored in the variable name
+console.log(name);
+
+// Change the value of the variable name again to your mother's name
+name = "Radha";
+
+// Print the final value stored in the variable name
+console.log(name);
