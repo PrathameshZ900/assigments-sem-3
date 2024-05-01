ef8ff1472d1dce4302d28a04a57fec5059d69ad9
prathamesh magar <prathameshma1@gmail.com>
2024-05-01 15:30:40 +0530

    OK done Question 2 as well

---
+++ day-9/question2.js
@@ -0,0 +1,41 @@
+function isPrime(number) {
+    if (number <= 1) {
+        return false;
+    }
+    
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+
+function PrimeNumbers(number) {
+    let ans = [];
+    for(let i = 2; i <= number; i++){
+        if(isPrime(i)){
+            ans.push(i);
+        }
+    }
+    return ans;
+}
+
+function checkPrimeNumber(input) {
+    let number = parseInt(input);
+    console.log(`This are Prime Numbers Form 0 to ${number} :`,PrimeNumbers(number));
+    
+}
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question("Enter a number to check if it's prime: ", (input) => {
+    checkPrimeNumber(input);
+    readline.close();
+});
+
