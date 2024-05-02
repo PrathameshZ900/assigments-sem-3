485855bb1c5deace4fcd350f834cd1a0852382b5
prathamesh magar <prathameshma1@gmail.com>
2024-05-03 00:20:06 +0530

    ok done:

--- day-10/question1.js
+++ day-10/question1.js
@@ -1,3 +1,9 @@
+// Problem-1 Rectangle Object
+
+// Create a rectangle object with length and width properties
+// Create two methods area and perimeter, that will return the area and perimeter of the rectangle
+
+
 const rectangle = {
     length: 0,
     width: 0,
